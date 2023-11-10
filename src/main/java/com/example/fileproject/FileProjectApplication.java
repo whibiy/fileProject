f6da8213627627300f639c88bdc086b987613085
whibiy <wwwdotkr@gmail.com>
2023-11-10 09:45:29 +0900

    Initial commit

---
+++ src/main/java/com/example/fileproject/FileProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fileproject/FileProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
